1a76c412693fcd49dbb2952ac63607b1c578396a
Alexandre Aimbiré <alexvickers@gmail.com>
2017-05-30 17:04:54 -0300

    Let's Rock!

---
+++ README.md
@@ -0,0 +1,15 @@
+# StarterPack
+
+// Pacote Básico para começar um website em Bootstrap. *Lean and Mean*.
+
+// O que tem aqui:
+
+// Index (header.php, index.php e footer.php)
+// Bootstrap 3.3.6
+// Open Sans - Google Fonts
+// bxSlider 4 Rahisified
+// Lightbox
+// jQuery 2.2.4
+// style.css
+// normalize.css - minified
+// mediqueries.css
---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer>
+</footer>
+<!--jQuery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<!--Bootstrap-->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!--bxSlider4 Rahisified-->
+<script src="js/jquery.bxslider-rahisified.min.js"></script>
+<!--Lightbox-->
+<script src="js/ekko-lightbox.min.js"></script>
+<script>
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+
+</script>
+<!--Plugins-->
+<script src="js/plugins.js"></script>
+</body>
+
+</html>
