c75a3fb63550c3a22f80546b1340dbce2cbab4ff
Alexandre Aimbiré <alexvickers@gmail.com>
2017-05-31 16:55:53 -0300

    Um montãozão de coisas.

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
     <meta name="keywords" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <!-- FIXME: Inserir Título-->
     <title></title>
 
     <!--Favicon-->
@@ -14,15 +15,39 @@
     <!--Botstrap-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <!--Estilos-->
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/five.css">
+    <link rel="stylesheet" type="text/css" href="css/style.min.css">
     <link rel="stylesheet" type="text/css" href="css/mediaqueries.css">
     <link rel="stylesheet" type="text/css" href="css/normalize.min.css">
     <!--Fonte-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-
 </head>
 
 <body>
     <header class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-1 col-sm-2 col-xs-4 logo relative">
+                    <img class="absolute" src="img/logo.png">
+                </div>
+                <div class="col-md-offset-1 col-md-8 hidden-sm hidden-xs">
+                    <nav>
+                        <ul>
+                            <li>A Fundação</li>
+                            <li>Matriz / Unidades</li>
+                            <li>Fotos</li>
+                            <li>Doar</li>
+                            <li>Blog
+                            <li>Contato</li>
+                        </ul>
+                    </nav>
+                </div>
+                <!--TODO: Arrumar Links-->
+                <div class="col-md-2 hidden-sm hidden-xs">
+                    <div class="social">
+                        <a href="#" target="_blank"></a>
+                    </div>
+                </div>
+                <!--TODO: Menu Mobile-->
+            </div>
+        </div>
     </header>
--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php include 'header.php';?>
 <main>
-    <section class="banner">
-        <ul class="bxslider" data-call="bxslider" data-options="autoReload:true, oneToOneTouch:false">
+    <section class="container-fluid banner relative no-left no-right">
+        <ul class="bxslider" data-call="bxslider" data-options="autoReload:true, oneToOneTouch:false, controls:false, pager:false">
+            <!--FIXME: Slides invadindo o anterior-->
+            <li>
+                <div class="slide" style="background-image: url('img/banner.jpg')">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 col-md-offset-6">
+                                <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
         </ul>
+        <div class="overlay absolute"></div>
     </section>
 </main>
 <?php include 'footer.php';?>
