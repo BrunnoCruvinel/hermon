3b2f3754a27cb6a4472240cbc35aafda9227ffea
Brunno Cruvinel <brunnocruvinel@gmail.com>
2017-07-17 17:32:04 -0300

    blog + sidebar

--- blog.php
+++ blog.php
@@ -1,26 +1,63 @@
 <?php include 'header.php';?>
-<main>
-    <div class="container">
+  <main>
+    <?php $cor = "laranjao"; include 'contribuir.php';?>
+
+      <section class="container blog">
         <div class="row">
-            <div class="col-md-8 col-md-offset-4 laranjao">
-                <?php include 'contribuir.php';?>
-            </div>
+          <div class="col-md-12">
+            <h2>Blog</h2>
+          </div>
         </div>
-    </div>
-    <section class="container blog">
-        <div class="row">
-            <div class="col-md-12">
-                <h2>Blog</h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-9 roll">
+        <div class="row nopadding">
+          <div class="col-md-9 nopadding">
+
+            <!-- NOTICIA -->
+            <div class="col-md-12 noticia">
+              <div class="col-md-4 nopadding">
+                <a href="single.php">
+                  <h3 class="cb">Lorem adipisicing elit, sed do eiusmod tempor incididunt ut </h3>
+                  <p>cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </a>
+              </div>
+              <div class="col-md-7">
+                <img src="img/blog.png">
+              </div>
             </div>
 
-            <?php include 'sidebar.php';?>
+            <!-- NOTICIA -->
+            <div class="col-md-12 noticia">
+                <a href="single.php">
+                  <h3 class="cb">Lorem adipisicing elit, sed do eiusmod tempor incididunt ut </h3>
+                  <p>cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </a>
+            </div>
+
+            <!-- NOTICIA -->
+            <div class="col-md-12 noticia">
+                <a href="single.php">
+                  <h3 class="cb">Lorem adipisicing elit, sed do eiusmod tempor incididunt ut </h3>
+                  <p>cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </a>
+            </div>
+            
+            <!-- NOTICIA -->
+            <div class="col-md-12 noticia">
+              <div class="col-md-4 nopadding">
+                <a href="single.php">
+                  <h3 class="cb">Lorem adipisicing elit, sed do eiusmod tempor incididunt ut </h3>
+                  <p>cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </a>
+              </div>
+              <div class="col-md-7">
+                <img src="img/blog.png">
+              </div>
+            </div>
+
+          </div>
+          <?php include 'sidebar.php';?>
 
         </div>
-    </section>
-</main>
+      </section>
+  </main>
 
-<?php include 'footer.php';?>
+  <?php include 'footer.php';?>
--- sidebar.php
+++ sidebar.php
@@ -1,25 +1,25 @@
-<div class="col-md-3 col-md-offset-1">
+<div class="col-md-3 nopadding">
     <div class="pesquisar">
-        <form>
-            <input type="text" placeholder="Pesquisar">
-            <input type="submit" value="">
+        <form id="pesquisar" method="get" action="">
+            <input  type="text" name="s" placeholder="Pesquisar">
+            <button type="submit"><span class="glyphicon glyphicon-search"></span></button>
         </form>
     </div>
     <div class="cadastre">
         <h3><b>Cadastre-se</b> para receber as novidades!</h3>
-        <form>
-            <input type="email" placeholder="E-mail">
-            <input type="submit" value="Enviar">
+        <form><br>
+            <input type="email" placeholder="E-mail"><br>
+            <input type="submit" value="Cadastrar">
         </form>
     </div>
     <div class="lidas">
-        <h3>Mais Lidos</h3>
+        <h3 class="cm">Mais Lidos</h3>
         <a href="single.php"><h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</h4></a>
         <a href="single.php"><h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</h4></a>
         <a href="single.php"><h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</h4></a>
     </div>
     <div class="categorias">
-        <h3>Categorias</h3>
+        <h3 class="cm">Categoria</h3>
         <ul>
             <li><a href="#">Florianópolis</a></li>
             <li><a href="#">Palhoça</a></li>
